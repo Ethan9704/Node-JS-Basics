b2c5d0b3d98b06b9aba76d0f2b4d0edd845d92df
Magomed <emplfors@gmail.com>
2024-09-04 23:10:48 +0200

    Route parameters

--- 01-express/01-basic-express/app.js
+++ 01-express/01-basic-express/app.js
@@ -3,12 +3,16 @@ const express = require('express');
 const app = express();
 
 const getRootHandler = (req, res) => {
-  res.send('Response From Express');
+  res.send('Get root route');
 };
 
 const getCommentsHandler = (req, res) => {
   res.send('Get comments route');
 };
+
+const getCommentHandler = (req, res) => {
+  res.send(`Get comment route. CommentId ${req.params.commentId}`)
+}
 
 const postCommentsHandler = (req, res) => {
   res.send('Post comments route');
@@ -16,6 +20,7 @@ const postCommentsHandler = (req, res) => {
 
 app.get('/', getRootHandler);
 app.get('/comments', getCommentsHandler);
+app.get('/comments/:commentId', getCommentHandler)
 app.post('/comments', postCommentsHandler);
 
 app.listen(5000, () => console.log('Server was started on port 5000'));
